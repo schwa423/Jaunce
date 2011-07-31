27bcc7024c4ac1ab63f428bc11367f4c4c109d14
Josh Gargus <josh@schwa.ca>
2011-07-31 11:05:34 -0700

    Proof-of-concept for transforming 2D events into Page coordinate system.  First transform mouse position into 3D ray, then intersect ray with Page mesh, and finally convert the intersection position into the Page's inner coordinate-system (i.e. take pan/zoom/etc. into account).  For now, just adds a new blue cube at the click position.

--- js/sketch/Page.js
+++ js/sketch/Page.js
@@ -3,7 +3,6 @@
 // Used in definition of Jaunce Page class.
 var initCamera = function() {
 	var camera = new THREE.Camera();
-	camera.projectionMatrix = THREE.Matrix4.makeOrtho(-500, 500, 500, -500, -10000, 10000);
 	camera.position.z = 1000;
 	return camera;
 };
@@ -12,16 +11,16 @@ var initCamera = function() {
 var initScene = function() {
 	var scene = new THREE.Scene();
 
-	var cubeGeometry = new THREE.CubeGeometry(10, 10, 10);
+	var cubeGeometry = new THREE.CubeGeometry(0.8, 0.8, 0.8);
 	var cube1 = new THREE.Mesh(cubeGeometry, new THREE.MeshLambertMaterial({ color: 0xff0000 }));	
-	cube1.position.x = -0.5;
-	cube1.position.y = -0.5;
+	cube1.position.x = -0.3;
+	cube1.position.y = -0.3;
 	cube1.position.z = 0;
 	scene.addObject(cube1);
 
 	var cube2 = new THREE.Mesh(cubeGeometry, new THREE.MeshLambertMaterial({ color: 0x00ff00 }));	
-	cube2.position.x = 0.5;
-	cube2.position.y = 0.5;
+	cube2.position.x = 0.3;
+	cube2.position.y = 0.3;
 	cube2.position.z = 0;
 	scene.addObject(cube2);
 
@@ -40,6 +39,12 @@ var initRenderTarget = function() {
 	);
 };
 
+
+// Design goals:
+// - When you make a page bigger or smaller, the content on it is scaled accordingly.  For
+//   example, if you have a page in a 3d space and you grow it to be as tall as a building,
+//   then the drawings on it as similarly large.  If you want to zoom into the Page contents,
+//   then you must do this manually.
 Class('Page', {
 	meta: Joose.Meta.Class,
 	// Subclass THREE.Mesh so that we can be inserted directly into a THREE.Scene.
@@ -48,27 +53,54 @@ Class('Page', {
 	has: {
 		camera: { is: 'r', init: initCamera },
 		scene: { is: 'r', init: initScene },
-		renderTarget: { is: 'r', init: initRenderTarget }
+		renderTarget: { is: 'r', init: initRenderTarget },
+		width: 1,
+		height: 1
 	},
 	methods: {
 		initialize: function(props) {
-			var geometry = new THREE.PlaneGeometry(3, 2, 10, 10);
+			var geometry = new THREE.PlaneGeometry(this.width, this.height, 10, 10);
 			var material = new THREE.MeshBasicMaterial({ map: this.renderTarget });
+		
+			var w = this.width, h = this.height;
+			this.width = this.height = null;  // otherwise nothing will happen; see guard at start of setWidthAndHeight()
+			this.setWidthAndHeight(w,h);
+
 			THREE.Mesh.call(this, geometry, material);
 		},
 
-		update: function(parentMatrixWorld, forceUpdate, camera, renderer) {
+		// For now, can only be called at creation-time.  To do it at a later time requires more
+		// investigation.  Should we just create new geometry?  Should we just scale the existing
+		// geometry?
+		setWidthAndHeight: function(newWidth, newHeight) {
+			if (this.width === newWidth && this.height === newHeight) return;  // nothing changed
+			this.width = newWidth;
+			this.height = newHeight;
+
+			// Our smaller dimension (width/height) determines the scale for the page content.
+			var aspect = newWidth / newHeight;
+			this.camera.projectionMatrix =
+				aspect > 1 
+				? THREE.Matrix4.makeOrtho(-1*aspect, 1*aspect, 1, -1, -10000, 10000 )
+				: THREE.Matrix4.makeOrtho(-1, 1, 1/aspect, -1/aspect, -10000, 10000 )
+		},
+
+		// This requires a modified version of Three.js... updating ourself involved
+		// potentially rendering into our texture, and in order to do this we need
+		// access to a renderer.
+		update: function(parentMatrixWorld, forceUpdate, outerCamera, renderer) {
 			if (!this.lastUpdateTime || (Date.now() - this.lastUpdateTime) > 100) {
 				this.lastUpdateTime = Date.now();
-// NOT QUITE READY... NEEDS HACKED VERSION OF THREE.JS				
-//				renderer.render(this.scene, this.camera, this.renderTarget, true);
+				
+				// Render the updated page contents.
+				renderer.render(this.scene, this.camera, this.renderTarget, true);
 			}
 
-			this.SUPER(parentMatrixWorld, forceUpdate, camera, renderer);
+			// Countinue updating ourself just like any other THREE.Mesh; we'll soon be 
+			// rendered using the texture containing the rendered page-contents.
+			this.SUPER(parentMatrixWorld, forceUpdate, outerCamera, renderer);
 		}
 	}
 });
-
-
 
 })();
